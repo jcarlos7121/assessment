bf74705e9f50ad105e38e994d1ed62acb84bc355
jcarlos7121 <jchinojob7@gmail.com>
2014-04-10 22:47:35 -0500

    added, some styles, finished...

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,33 +4,42 @@ $(document).ready(function() {
 		event.preventDefault();
 		var input = $(".first").val();
 		if (isNaN(input)) {
-			$('.result').html("Hey! that was not a number!");
+			$('.result').html("<h4 id='faderesult' style='display:none;'>Hey! That was not a number!</h4>");
+			setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
 		}else{
 			console.log('entreaca');
 			$.post('/firstmethod', { number:input,language:$('#language').val()}, function(data, textStatus, xhr) {
 				console.log(data);
-				$('.result').html(data);
+				$('.result').html("<h4 id='faderesult' style='display:none;'>"+data+"</h4>");
+				setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
 			});
 		}
 	});
 	$('.first').on('input',function  () {
 		var input = $(this).val();
 		if (isNaN(input)) {
-			$('.result').html("Hey! that was not a number!");
+			$('.result').html("<h4 id='faderesult' style='display:none;'>Hey! That was not a number!</h4>");
+			setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
 		}else{
 			console.log('entreaca');
 			$.post('/firstmethod', { number:input,language:$('#language').val()}, function(data, textStatus, xhr) {
-				console.log(data);
-				$('.result').html(data);
+				$('.result').html("<h4 id='faderesult' style='display:none;'>"+data+"</h4>");
+				setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
 			});
 		}
 	});
 
 	$('.second').on('input', function(event) {
 		event.preventDefault();
-		inputnumber = $(this).val()
-		$.post('/secondmethod',{number:inputnumber}, function(data, textStatus, xhr) {
-			$('.result').html(data);
-		});
+		inputnumber = $(this).val();
+		if (isNaN(inputnumber)) {
+			$('.result').html("<h4 id='faderesult' style='display:none;'>Hey! That was not a number!</h4>");
+			setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
+		}else{
+			$.post('/secondmethod',{number:inputnumber}, function(data, textStatus, xhr) {
+				$('.result').html("<h4 id='faderesult' style='display:none;'>"+data+"</h4>");
+				setTimeout(function() { $('#faderesult').fadeIn("slow"); }, 100);
+			});
+		}
 	});
 });
