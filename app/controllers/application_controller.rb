7e9a2621e95ed974bc7622a2f98ac44224dab7c6
jcarlos7121 <jchinojob7@gmail.com>
2014-04-10 23:14:24 -0500

    Added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < ActionController::Base
 
     #i declared an array with numbers less than 20
     lessthantenth = %W[one two three four five six seven eight nine ten eleven twelve thirten fourteen fifteen sixteen seventeen eighteen nineteen].unshift(' ')
+    #i declared an array for the tenths
     tenths = %W[twenty thirty fourty fifty sixty seventy eighty ninety]
 
     input = params[:number]
@@ -40,20 +41,29 @@ class ApplicationController < ActionController::Base
 
     result = ""
 
+    #if the number is less than 20, i just look inside the array.
     if number < 20
       result = lessthantenth[number]
+
+      #if the number its between 19 and 100 i just find the match inside the tenths and the last number and append it to the result string
     elsif number > 19 && number < 100
       result = tenths[Integer(number.to_s[0])-2] + " " + lessthantenth[Integer(number.to_s[1])]
+    
+      #i just make use of the first numbers and append 'hundred', along with the previous matching inside the arrays.
     elsif number > 99 && number < 1000
         if Integer(number.to_s[1]) == 0
           result = lessthantenth[Integer(number.to_s[0])] + " hundred " + " " + lessthantenth[Integer(number.to_s[2])]   
         else
+          #catch for the "teens", nineteen, seventeen, etc.
           if Integer(number.to_s[1]) == 1
            result = lessthantenth[Integer(number.to_s[0])] + " hundred " + lessthantenth[Integer(number.to_s[1]+number.to_s[2])] 
+          #if there are none of them, then i just match the tenths with its last numbers.
           else
             result = lessthantenth[Integer(number.to_s[0])] + " hundred " + tenths[Integer(number.to_s[1])-2] + " " + lessthantenth[Integer(number.to_s[2])]            
           end
         end
+
+   #if the number its in between 999 and 10000, then just continue matching
     elsif number > 999 && number < 10000
       #1000
       if Integer(number.to_s[1]) == 0
